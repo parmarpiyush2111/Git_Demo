06f87911f2a4d81eb37b6e64f476ddcda8eb430a
Parmar Piyush <parmarpiyush2111@gmail.com>
2024-03-19 12:45:26 +0530

    C Coding Complete

---
+++ First.c
@@ -0,0 +1,10 @@
+
+#include<stdio.h>
+
+int main(){
+    printf("Hello World\n");
+    printf("My Name is Piyush Parmar\n");
+    printf("I am Studying MSC IT Course, & i am learning C language Right Now !!!\n");
+
+    return 0;
+}
