5e0ad2c05c6b050276a35c39d180bfc91281d65c
Parmar Piyush <parmarpiyush2111@gmail.com>
2024-03-19 12:45:53 +0530

    JAVA Coding Complete

---
+++ FirstJava.java
@@ -0,0 +1,9 @@
+/**
+ * FirstJava
+ */
+public class FirstJava {
+    public static void main(String []args){
+        System.out.println("Hello World");
+        System.out.println("my first java Program !!!");
+    }
+}
